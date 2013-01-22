25c69cc64d95689712f0729991762d1640449db9
zhougf <541430660@qq.com>
2013-01-22 10:07:45 +0800

    update app.yaml & mm2md.py

--- FreeMindTools/mm2md.py
+++ FreeMindTools/mm2md.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from __future__ import unicode_literals
 from xml.etree import ElementTree
 import os
-import conf4BlogListInGithub
+import sys
+import yaml
+
 
 
 class MMTransform():
@@ -87,14 +90,21 @@ class MMTransform():
 
 class MakeBlogInGithub():
     """make blog by markdown file in github.com"""
-    def _getconf(self,mdFilename):
+    def _getconfFromPy(self,mdFilename):
         mdfile = 'default'
         if  conf4BlogListInGithub.bloglist.has_key(mdFilename):
             mdfile = mdFilename
         return conf4BlogListInGithub.bloglist[mdfile]
 
-    def md2blog(self, md, mdFilename):
-        config = self._getconf(mdFilename)
+    def _getconfFromYaml(self,configFilename,mdFilename):
+        mdfile = 'default'
+        config = file(configFilename,'rb')
+        config.close()
+        if  config.has_key(mdFilename):
+            mdfile = mdFilename
+        return config[mdfile]
+
+    def md2blog(self, md, config, mdFilename):
         prefix = []
         prefix.append('---')
         prefix.append('layout : '+config['layout'])
@@ -102,31 +112,54 @@ class MakeBlogInGithub():
         prefix.append('tags : [' + ', '.join(config['tags'].split(',')) + ']')
         prefix.append('title : ' + config['title'])
         prefix.append('---')
-        prefix.append('[阅读思维导图]('+config['mmLink']+')')
+        prefix.append('[思维导图文件下载]('+config['mmLink']+')')
         prefix.append(md)
         return os.linesep.join(prefix)
 
-def main():
-    mmdir = u'D:\Python26\FreeMindTools-master'
-    # mddir = '/home/rain/download'
-    mddir = u'C:\Documents and Settings\Administrator\桌面'
+def usage():
+    print '''
+    '''
+
+def getconf(conf_dic, title):
+    key = 'default.mm'
+    if conf_dic.has_key(title):
+        key = title
+    return conf_dic[key]
+
+
+def main(argv):
+    confile = file('app.yaml','rb')
+    config = yaml.load(confile)
+    dirconf = 'file_dir'
+    mmdir = config[dirconf]['mm']
+    mddir = config[dirconf]['md']
+
     mblog = MakeBlogInGithub()
-    mmFilename = u'大量阅读的重要性.mm'
-    textileFilename = 'textile.txt'
-    mdFilename = mblog._getconf(mmFilename)['mdfname']
-
-    mm = file(os.path.join(mmdir,mmFilename),'rb')
-    md = file(os.path.join(mddir,mdFilename),'wb')
-    textile = file(os.path.join(mddir,textileFilename),'wb')
-    
     transform = MMTransform()
-    md.write(mblog.md2blog(transform.mm2md(mm.read()).encode('utf8'), mmFilename))
-    # textile.write(transform.mm2textile(mm.read()).encode('utf8'))
-    mm.close()
-    md.close()
-    textile.close()
 
-    
+    if len(argv) > 1:
+        file_list = [argv[1]]
+    else:
+        file_list = [f_name for f_name in os.listdir(mmdir) if f_name.decode('utf8').endswith('mm')]
+
+    for f_in_name in file_list:
+        conf = getconf(config,f_in_name.decode('utf8'))
+        mmFilename = f_in_name
+        mdFilename = conf['mdfname']
+        mm = file(os.path.join(mmdir,mmFilename),'rb')
+        md = file(os.path.join(mddir,mdFilename),'wb')
+        mdContent = transform.mm2md(mm.read())
+        blogContent = mblog.md2blog(mdContent,conf,mdFilename)
+        md.write(blogContent.encode('utf8'))
+        mm.close()
+        md.close()
+        print os.path.join(mddir,mdFilename) + ' is OK!'
+    confile.close()
+    # textileFilename = 'textile.txt'
+    # textile = file(os.path.join(mddir,textileFilename),'wb')
+    # textile.write(transform.mm2textile(mm.read()).encode('utf8'))
+    # textile.close()
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
+    # main([])
